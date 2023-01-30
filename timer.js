b6f98c70f4a815cff4b2522ad8c1b6d238c3456f
Assad Naqvi <assadnaqvi72@gmail.com>
2023-01-30 22:16:00 +0500

    added changes

---
+++ timer.js
@@ -0,0 +1,7 @@
+let date = Date();
+function Date() {
+    return Date;
+}
+console.log(Date);
+Date();
+export {};
---
+++ timer.ts
@@ -0,0 +1,9 @@
+import inquirer from "inquirer";
+let date:{
+    name:string;
+}=Date()
+function Date(){
+    return Date;
+}
+console.log(Date)
+Date();
